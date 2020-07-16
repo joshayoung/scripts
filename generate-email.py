4bc688c1147f568334b7b40c69384baecc9fdfba
Josh Young <j22cal@gmail.com>
2020-07-16 15:41:50 -0500

    Change email generator to allow a configurable name and host

--- generate-email.py
+++ generate-email.py
@@ -1,10 +1,21 @@
 #! /usr/bin/python
+import sys
 from datetime import datetime
 from subprocess import Popen, PIPE
 
+try:
+    email_name = sys.argv[1]
+except IndexError:
+    email_name = "example"
+
+try:
+    email_host = sys.argv[2]
+except IndexError:
+    email_host = "example.com"
+
 def generate_email():
     rdate = datetime.now().strftime("%Y%d%m%H%M%S")
-    return "example" + rdate + "@example.com"
+    return email_name + rdate + "@" + email_host
 
 def output_email():
     email = generate_email()
